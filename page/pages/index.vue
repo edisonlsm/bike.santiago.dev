7ef595b4dad4fdc9c8d3233307d538f3aa80aa52
Edison Santiago <edisonluizsm@gmail.com>
2021-09-19 17:33:58 -0300

    First commit

---
+++ page/pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    {{ strava.stats }}
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData ({ $axios }) {
+    const strava = await $axios.$get('https://santiago-bike-stats.santiago.workers.dev/')
+
+    return { strava }
+  }
+}
+</script>
