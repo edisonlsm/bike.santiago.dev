8c6fd623baef808025149e5ecc21870d092e2939
Edison Santiago <edisonluizsm@gmail.com>
2025-01-02 21:36:00 -0300

    Debug logging

--- server/api/test.get.ts
+++ server/api/test.get.ts
@@ -1,4 +1,6 @@
 export default defineEventHandler(async (event) => {
+  console.log(Object.keys(event.context));
+  console.log(Object.keys(useRuntimeConfig(event)));
   return {
     hello: 'world',
     keys: Object.keys(event.context)
