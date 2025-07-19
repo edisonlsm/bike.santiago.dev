f69e979d59a9b37efbfbc29e35325360f249d9ff
Edison Santiago <edisonluizsm@gmail.com>
2025-07-19 10:00:21 -0300

    debug get longestActivity

--- server/api/strava/activities/[id].get.ts
+++ server/api/strava/activities/[id].get.ts
@@ -2,13 +2,19 @@ import type { Strava } from '~/types/strava';
 import { z } from 'zod'
 
 export default defineEventHandler(async (event) => {
+  console.log(`defineEventHandler`)
+
   const query = await getValidatedQuery(event, z.object({
     access_token: z.string(),
   }).parse);
 
+  console.log(`defineEventHandler | query`)
+
   const params = await getValidatedRouterParams(event, z.object({
     id: z.string()
   }).parse);
+
+  console.log(`defineEventHandler | params`)
 
   console.log(`longestActivityId: ${params.id}`)
 
